d424c4af8ad1c9a5c30a666bef3b54f1cdf06c88
Vincent <Vincentnguyen030@gmail.com>
2023-06-05 14:54:35 -0500

    Create book function that takes 4 parameters

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+let myLibrary = [];
+
+function Book(title,author,pages,status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
+  }
+}
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook)
+//   write code to loop through library and add display
+}
+
+// Whenever press submit button, apply Book function and add  to library
+// After every submission, loop through library and add new cards
+
+const submit = document.querySelector
